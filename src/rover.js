6dcad101f5be50be270cec52662660ae8d0e7d1e
nanshan231 <nanshan231@gmail.com>
2022-03-01 15:18:04 +0800

    [feat] Add move function for rover

--- src/rover.js
+++ src/rover.js
@@ -16,15 +16,29 @@ export default class Rover {
   }
 
   turnRight() {
-    this.location.direction = this.getNextDirection(this.location.direction, true);
+    this.location.direction = this.getNextDirection(
+      this.location.direction,
+      true
+    );
   }
 
   getNextDirection(direction, isClosewise) {
-    const directions = ['N', 'W', "S", 'E'];
+    const directions = ["N", "W", "S", "E"];
     const index = directions.indexOf(this.location.direction);
     if (isClosewise) {
       return directions[index - 1] || directions[directions.length - 1];
     }
     return directions[index + 1] || directions[0];
+  }
+
+  move() {
+    const moveDistance = {
+      N: { x: 0, y: 1 },
+      S: { x: 0, y: -1 },
+      E: { x: 1, y: 0 },
+      W: { x: -1, y: 0 },
+    };
+    this.location.x += moveDistance[this.location.direction].x;
+    this.location.y += moveDistance[this.location.direction].y;
   }
 }
--- src/rover.test.js
+++ src/rover.test.js
@@ -9,11 +9,11 @@ describe("Mars rover", () => {
   })
 
   test.each([
-    { location: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'W'} },
-    { location: {x: 0, y: 0, direction: 'W'}, expected: {x: 0, y: 0, direction: 'S'} },
-    { location: {x: 0, y: 0, direction: 'S'}, expected: {x: 0, y: 0, direction: 'E'} },
-    { location: {x: 0, y: 0, direction: 'E'}, expected: {x: 0, y: 0, direction: 'N'} },
-  ])("Should follow rules %j when rover turns left", ({ location, expected }) => {
+    {location: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'W'}},
+    {location: {x: 0, y: 0, direction: 'W'}, expected: {x: 0, y: 0, direction: 'S'}},
+    {location: {x: 0, y: 0, direction: 'S'}, expected: {x: 0, y: 0, direction: 'E'}},
+    {location: {x: 0, y: 0, direction: 'E'}, expected: {x: 0, y: 0, direction: 'N'}},
+  ])("Should follow rules %j when rover turns left", ({location, expected}) => {
     const rover = new Rover();
     rover.setLocation(location);
     rover.turnLeft();
@@ -21,14 +21,26 @@ describe("Mars rover", () => {
   })
 
   test.each([
-    { location: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'E'} },
-    { location: {x: 0, y: 0, direction: 'W'}, expected: {x: 0, y: 0, direction: 'N'} },
-    { location: {x: 0, y: 0, direction: 'S'}, expected: {x: 0, y: 0, direction: 'W'} },
-    { location: {x: 0, y: 0, direction: 'E'}, expected: {x: 0, y: 0, direction: 'S'} },
-  ])("Should follow rules %j when rover turns right", ({ location, expected }) => {
+    {location: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'E'}},
+    {location: {x: 0, y: 0, direction: 'W'}, expected: {x: 0, y: 0, direction: 'N'}},
+    {location: {x: 0, y: 0, direction: 'S'}, expected: {x: 0, y: 0, direction: 'W'}},
+    {location: {x: 0, y: 0, direction: 'E'}, expected: {x: 0, y: 0, direction: 'S'}},
+  ])("Should follow rules %j when rover turns right", ({location, expected}) => {
     const rover = new Rover();
     rover.setLocation(location);
     rover.turnRight();
+    expect(rover.getLocation()).toEqual(expected);
+  })
+
+  test.each([
+    {location: {x: 1, y: 1, direction: 'N'}, expected: {x: 1, y: 2, direction: 'N'}},
+    {location: {x: 1, y: 1, direction: 'E'}, expected: {x: 2, y: 1, direction: 'E'}},
+    {location: {x: 1, y: 1, direction: 'W'}, expected: {x: 0, y: 1, direction: 'W'}},
+    {location: {x: 1, y: 1, direction: 'S'}, expected: {x: 1, y: 0, direction: 'S'}},
+  ])("Should follow rules %j when rover moves", ({ location, expected }) => {
+    const rover = new Rover();
+    rover.setLocation(location);
+    rover.move();
     expect(rover.getLocation()).toEqual(expected);
   })
 })
